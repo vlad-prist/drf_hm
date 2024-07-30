e1b0cfb2d020b3c2675cee9675b611dad14f37ac
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-30 15:24:46 +0300

    Добавлен в сериализатор счетчик уроков в курсе

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,9 +4,14 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons_count(self, obj):
+        return obj.lesson_set.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
