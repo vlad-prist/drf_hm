b925a3be01f2e667c200bded975c455c7bf810f5
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-31 13:52:18 +0300

    Добавлена новая модель Payment в приложение users, созданы две сущности новой модели

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from materials.models import Course, Lesson
 
 NULLABLE = {'blank': True, 'null': True}
 
@@ -20,3 +21,25 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_CASH = 'cash'
+    PAYMENT_TRANSFER = 'transfer'
+    PAYMENT_CHOICES = (
+        (PAYMENT_CASH, 'Оплата наличными'),
+        (PAYMENT_TRANSFER, 'Безналичная оплата'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    date_payment = models.DateTimeField(verbose_name='дата оплаты', auto_now_add=True)
+    lesson_paid = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='оплаченный урок', **NULLABLE)
+    course_paid = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='оплаченный курс', **NULLABLE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='сумма оплаты')
+    payment_type = models.CharField(max_length=50, choices=PAYMENT_CHOICES, verbose_name='тип оплаты')
+
+    class Meta:
+        verbose_name = 'оплата'
+        verbose_name_plural = 'оплаты'
+
+    def __str__(self):
+        return f'{self.user.email} - {self.lesson_paid.title if self.lesson_paid else self.course_paid.title}'
