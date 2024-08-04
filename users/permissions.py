98c2e74689c12891267dfeaf6a2271b89138923d
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-08-04 10:43:06 +0300

    докстринг к пермишинс

--- users/permissions.py
+++ users/permissions.py
@@ -2,16 +2,13 @@ from rest_framework import permissions
 
 
 class IsModerator(permissions.BasePermission):
-
+    """Проверка на модератора."""
     def has_permission(self, request, view):
         return request.user.groups.filter(name="Модераторы").exists()
 
 
 class IsOwner(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
+    """Проверка на автора."""
 
     def has_object_permission(self, request, view, obj):
         if obj.owner == request.user:
