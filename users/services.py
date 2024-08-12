f442c082f5b8369c52d71221c36537a6f30cd929
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-08-12 16:35:24 +0300

    Изменения в stripe.Product

--- users/services.py
+++ users/services.py
@@ -1,46 +1,31 @@
-import requests
 import stripe
-from rest_framework import status
-
 from config import settings
-from forex_python.converter import CurrencyRates
 
 
 stripe.api_key = settings.STRIPE_API_KEY
 
 
-# def convert_rub_to_usd(amount):
-#     ''' Конвертирует рубли в доллары '''
-#     c = CurrencyRates()
-#     rate = c.get_rate('RUB', 'USD')
-#     return int(amount * rate)
-
-
 def create_stripe_product(obj):
     """Создаем stripe продукт"""
-    title_product = f'{obj.course_paid}' if obj.course_paid else f'{obj.lesson_paid}'
-    stripe_product = stripe.Product.create(name=f"{title_product}")
-    return stripe_product.get('id')
+    title_product = obj.course_paid if obj.course_paid else obj.lesson_paid
+    stripe_product = stripe.Product.create(name=title_product)
+    return stripe_product.id
 
 
-def create_stripe_price(amount):
+def create_stripe_price(product_id, amount):
     ''' Создает цену в Stripe '''
     return stripe.Price.create(
+        product=product_id,
         currency="rub",
         unit_amount=int(amount)*100,
-        product_data={"name": "Payment"},
     )
 
 
 def create_stripe_session(price):
-    ''' Создает сессию и оплатув Stripe '''
+    ''' Создает сессию и оплату в Stripe '''
     session = stripe.checkout.Session.create(
         success_url="https://127.0.0.1:8000/",
         line_items=[{"price": price.get('id'), "quantity": 1}],
         mode="payment",
     )
-    # response = requests.get(
-    #     f'{settings.STRIPE_API}/v1/checkout/sessions/:{session.get('id')}'
-    # )
-    # print(response.json())
     return session.get('id'), session.get('url')
--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from users.serializers import PaymentSerializer, UserSerializer #, UserDetailSer
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
-from users.services import create_stripe_price, create_stripe_session
+from users.services import create_stripe_price, create_stripe_session, create_stripe_product
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -50,9 +50,9 @@ class PaymentCreateAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         payment = serializer.save(user=self.request.user)
-        # amount_in_dollar = convert_rub_to_usd(payment.amount)
-        price = create_stripe_price(payment.amount)
-        session_id, payment_link = create_stripe_session(price)
+        product_id = create_stripe_product(payment)
+        price_id = create_stripe_price(product_id, payment.amount)
+        session_id, payment_link = create_stripe_session(price_id)
         payment.session_id = session_id
         payment.link = payment_link
         payment.save()
